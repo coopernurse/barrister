9ee96aaa34ed6761ec902712ddd6af8848aecd14
James Cooper <james@bitmechanic.com>
2012-03-16 21:37:28 -0700

    Switch to xargs so exit codes are propegated

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,8 @@ set -e
 export PYTHONPATH=`pwd`
 
 # regular unit tests
-find ./barrister/test -name "*_test.py" -exec python {} \; -print
+# use xargs instead of -exec so that we get exit code propegation
+find ./barrister/test -name "*_test.py" -print | xargs -n1 python
 
 # conformance test suite
 cd conform; python conform_test.py
