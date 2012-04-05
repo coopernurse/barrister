a7908dac80ec271137f0431dd146ea7e03841012
James Cooper <james@bitmechanic.com>
2012-04-04 19:28:19 -0700

    move node setup _above_ run_tests.sh

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -2,18 +2,22 @@
 
 set -e
 
+# setup python
 if [ ! -d env ]
 then
   virtualenv env
 fi
 . env/bin/activate
 ./env/bin/pip install -r requirements.txt
-./run_tests.sh
 
+# setup node
 if [ -n "$BARRISTER_NODE" ]
 then
   npm install $BARRISTER_NODE
 fi
+
+# run tests
+./run_tests.sh
 
 # generate docs
 rm -rf doc
