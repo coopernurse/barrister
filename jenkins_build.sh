f5789252d39036bb2edc090e8eb61cdc2f75cecf
James Cooper <james@bitmechanic.com>
2012-04-04 19:26:31 -0700

    run npm install if BARRISTER_NODE set

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -10,6 +10,11 @@ fi
 ./env/bin/pip install -r requirements.txt
 ./run_tests.sh
 
+if [ -n "$BARRISTER_NODE" ]
+then
+  npm install $BARRISTER_NODE
+fi
+
 # generate docs
 rm -rf doc
 epydoc -q --parse-only -o doc --docformat=restructuredText --exclude=test barrister
