a213db2e457afebec0b252a09b48294121d35e30
James Cooper <james@bitmechanic.com>
2012-04-25 13:43:46 -0700

    bump v0.1.1c

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from distutils.core import setup
 
 setup(
     name='barrister',
-    version='0.1.1b',
+    version='0.1.1c',
     url='https://github.com/coopernurse/barrister',
     scripts=['bin/barrister'],
     packages=['barrister',],
