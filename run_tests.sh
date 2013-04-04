ba0b4b7f51b074e7eab7be0a1770f03c2cb9ddec
James Cooper <james@bitmechanic.com>
2013-04-04 09:00:23 -0700

    add test idl files

--- run_tests.sh
+++ run_tests.sh
@@ -8,5 +8,8 @@ export PYTHONPATH=`pwd`
 # use xargs instead of -exec so that we get exit code propegation
 find ./barrister/test -name "*_test.py" -print | xargs -n1 python
 
+# run script to test parsing various IDL files
+./idl_parse_test.sh
+
 # conformance test suite
 cd conform; rm -f *.out; python conform_test.py
