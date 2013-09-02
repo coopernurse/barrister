442cec9a9510752fd39f8986742e2feb70b7bbd2
James Cooper <james@bitmechanic.com>
2013-09-02 14:49:44 -0700

    rsync doc dir after build

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 set -e
+
 export GH=/home/james/github
 export BARRISTER=$GH/barrister
 export BARRISTER_JAVA=$GH/barrister-java
@@ -11,5 +13,7 @@ export GOPATH=/home/james/go
 export JAVA_HOME=/usr/local/java
 export PATH=$PATH:$GOROOT/bin:$JAVA_HOME/bin:/usr/local/maven/bin
 export NODE_PATH=$NODE_PATH:$BARRISTER/node_modules
+
 git pull
 ./jenkins_build.sh
+rsync -avz doc/ james@barrister.bitmechanic.com:/home/james/barrister-site/api/python/latest/
